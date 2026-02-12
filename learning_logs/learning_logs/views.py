1c7e41a0fa6eaba5acc010d4a6f42bf2265a0239
marvin tan <ymarvintan@gmail.com>
2026-02-12 13:31:46 +0800

    update

--- learning_logs/learning_logs/views.py
+++ learning_logs/learning_logs/views.py
@@ -1,26 +1,31 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.http import Http404
 # Create your views here.
 def index(request):
     """The home page for learning log"""
     return render(request, 'learning_logs/index.xhtml')
 
+@login_required
 def topics(request):
     """Topics page"""
     # query the database for topic objects and order them by the date_added field, assign to topics
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     # create an context and send to the template
     context = {'topics':topics}
     return render(request, 'learning_logs/topics.xhtml', context)
-
+@login_required
 def topic(request, topic_id):
     """Topic page"""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request,  'learning_logs/topic.xhtml', context)
-
+@login_required
 def new_topic(request):
     """Adding a new topic"""
     if request.method != 'POST':
@@ -30,12 +35,15 @@ def new_topic(request):
         # POST data submitted; process data
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            # we pass commit=False because we need to modify the new_topic before saving it to the database
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     # display a blank or invalid form
     context = {'form':form}
     return render(request, 'learning_logs/new_topic.xhtml', context)
-
+@login_required
 def new_entry(request, topic_id):
     """Adding a new entry to a topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -51,11 +59,13 @@ def new_entry(request, topic_id):
     # display a blank or invalid form
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.xhtml', context)
-
+@login_required
 def edit_entry(request, entry_id):
     """Edit a entry from a topic"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     if request.method != 'POST':
         form = EntryForm(instance=entry)
     else:
--- learning_logs/ll_project/settings.py
+++ learning_logs/ll_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'learning_logs'
+    'learning_logs',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
